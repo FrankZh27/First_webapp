f5dfed02fa372b03b817d1c52f5498ea67951aea
FrankZh27 <jzho@umich.edu>
2018-01-09 21:04:02 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var app = express();
+
+
+var db = null;
+var url = "mongodb://localhost:27017/mittens";
+
+MongoClient.connect(url, function(err, dbconn){
+	if(!err) {
+		console.log("We are connected");
+		db = dbconn;
+	}
+});
 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get('/greetings', function(req, res, next){
-	
-	return res.send(greetings);
+app.get('/greetings', function(req, res, next) {
+	db.collection('greetings', function(err, greetingsCollection) {
+		greetingsCollection.find().toArray(function(err, greetings) {
+			return res.json(greetings);
+		});
+	});
+	// db.collection('greetings', function(err, greetingsCollection) {
+	// 	greetingsCollection.find().toArray(funciton(err, greetings){
+	// 		return res.json(greetings);
+	// 	});
+
+	// });
 });
 
 app.post('/greetings', function(req, res, next){
