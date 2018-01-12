c0707a34195b9e3680abb3978261adc84659698c
FrankZh27 <jzho@umich.edu>
2018-01-11 20:12:29 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 var app = express();
 
 
@@ -39,10 +41,67 @@ app.get('/greetings', function(req, res, next) {
 });
 
 app.post('/greetings', function(req, res, next){
-	greetings.push(req.body.newGreeting);
-	res.send();
+	db.collection('greetings', function(err, greetingsCollection) {
+		var newGreeting = {
+			text: req.body.newGreeting
+		}
+		
+		greetingsCollection.insert(newGreeting, {w:1}, function(err) {
+			return res.send();
+		});
+	});
+});
+
+
+app.put('/greetings/remove', function(req, res, next){
+	db.collection('greetings', function(err, greetingsCollection) {
+		var greetingId = req.body.greeting._id;
+console.log('got it!');
+		greetingsCollection.remove({_id: ObjectId(greetingId)}, {w:1}, function(err) {
+			return res.send();
+		});
+	});
+});
+
+app.post('/users', function(req, res, next){
+	db.collection('users', function(err, usersCollection) {
+		
+		bcrypt.genSalt(10, function(err, salt){
+			bcrypt.hash(req.body.password, salt, function(err, hash) {
+				var newUser = {
+					username: req.body.username,
+					password: hash
+				};
+
+				usersCollection.insert(newUser, {w:1}, function(err) {
+					return res.send();
+				});
+			});
+		});
+
+		
+	});
+});
+
+
+app.put('/users/signin', function(req, res, next){
+	
+	db.collection('users', function(err, usersCollection) {
+		
+		usersCollection.findOne({username: req.body.username}, function(err, user) {
+			
+			bcrypt.compare(req.body.password, user.password,function(err, result) {
+				if (result) {
+					return res.send();
+				} else {
+					return res.status(400).send();
+				}
+			});
+		});
+		
+	});
 });
 
 app.listen(3000, function(){
-	console.log('example');
+	console.log('Listening on port 3000');
 });
